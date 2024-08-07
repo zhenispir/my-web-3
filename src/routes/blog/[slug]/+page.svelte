3ca50a28ff1f11bd3e2b25ac357c6e16e697d620
Zhenis Pir <yenispiro@gmail.com>
2024-08-07 16:14:01 +0500

    Approah #2 is also finished and it worked.

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let data;
+</script>
+
+<article>
+	<h1>{data.title}</h1>
+	<p>Published: {data.date}</p>
+    <p>Categories: {data.category}</p>
+	<svelte:component this={data.content} />
+</article>
